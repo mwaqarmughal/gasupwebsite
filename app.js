a4338c2452813c832a8a260b85a0322566403787
Muhammad Waqar Mughal <56934353+mwaqarmughal@users.noreply.github.com>
2021-05-12 23:18:08 +0500

    react installed

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  const [product, setProduct]= useState([])
+  useEffect(() => {
+    axios.get('/api')
+    .then(
+      (res)=>{
+        setProduct(res.data)
+      }
+    )
+    .catch((err)=>{
+      console.log(err)
+    })
+    })
+
+  return(
+    <div className="App">
+      <ul>{product.map((p,index)=><li key="index">{p}</li>)}</ul>
+    </div>
+  );
+}
+
+export default App;
